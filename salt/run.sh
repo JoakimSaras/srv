65ad59ae83064ccdabf7d6234aa35ebc9c17335e
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 14:25:12 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -5,13 +5,12 @@ setxkbmap fi
 sudo timedatectl set-timezone Europe/Helsinki
 
 echo "Installing Salt-Minion and DropBox..."
+
 sudo apt-get update
 sudo apt-get -y install salt-minion
 
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 echo "Applying Settings..."
-
 echo 'master: localhost'|sudo tee /etc/salt/minion
-
 echo "Done."
