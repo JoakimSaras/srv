fc288bb3e4969e5aa297711065fb11b9f440c3f5
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 17:13:15 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Configuration starting..."
-
+setxkbmap fi
 echo "Installing updates and salt.."
 
 sudo apt-get update
@@ -11,5 +11,5 @@ echo "Installing salt"
 
 sudo apt-get -y install salt-minion
 
-echo "Ready.."
+echo "Ready..Please hold for instructions given from Master."
 
