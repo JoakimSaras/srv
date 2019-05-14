77244d51e319f175ae4e9d51440b5c51df8db2ff
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 15:09:53 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -6,4 +6,5 @@ sudo apt-get update
 sudo apt-get -y install salt-minion
 echo "Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
+echo 'id: koti'|sudo tee /etc/salt/minion
 echo "Done."
