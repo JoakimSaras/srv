ecb641d2a6e521c687577bb16b7b6a390a511bf7
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 17:27:21 +0300

    Update setuppi.sh

--- salt/setuppi.sh
+++ salt/setuppi.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-echo "laadilaa"
+echo "Configuring computer..."
 
-echo "laadilaa"
+echo "Installing updates and upgrades"
 
 sudo apt-get update
 
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
-echo "laalaa"
+echo "Install and configure salt"
 
 sudo apt-get -y install salt-minion
+
+echo 'master: localhost'|sudo tee /etc/salt/minion 
+
+echo 'id: koti'|sudo tee /etc/salt/minion 
 
