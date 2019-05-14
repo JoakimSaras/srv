f8ad275c2e79b9dd81d9098bf698b20f8b8ef96b
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 16:52:45 +0300

    Update run2.sh

--- salt/run2.sh
+++ salt/run2.sh
@@ -13,8 +13,8 @@ echo "Configuring necessary things for GIT.."
 
 git config --global credential.helper "cache --timeout=3600"
 git config --global push.default simple
-# git config --global user.email "joakim.saras@gmail.com"
-# git config --global user.name "Testi Testinen"
+git config --global user.email "joakim.saras@gmail.com"
+git config --global user.name "Testi Testinen"
 
 echo "More tips and tricks in GitHub folder / files. Use "cat filename" "
 
