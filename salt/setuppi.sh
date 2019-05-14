b104ce3043585134060d4718b30adf7fa587244d
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 17:33:12 +0300

    Update setuppi.sh

--- salt/setuppi.sh
+++ salt/setuppi.sh
@@ -12,7 +12,7 @@ echo "Install and configure salt"
 
 sudo apt-get -y install salt-minion
 
-echo 'master: localhost'|sudo tee /etc/salt/minion 
+echo 'master: localhost' 'id: koti'|sudo tee /etc/salt/minion 
 
-echo 'id: koti'|sudo tee /etc/salt/minion 
+echo "All done. Please wait for more instructions by Master!"
 
