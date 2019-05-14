3406ebb58ec1b2777601ef43f4263a7727a0ce4b
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 17:16:15 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -7,9 +7,10 @@ echo "Installing updates and salt.."
 sudo apt-get update
 sudo apt-get upgrade
 
-echo "Installing salt"
+echo "Installing and configuring salt..."
 
 sudo apt-get -y install salt-minion
+echo 'master: localhost'|sudo tee /etc/salt/minion
 
 echo "Ready..Please hold for instructions given from Master."
 
