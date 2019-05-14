55fe7a7c2d1e3415be0105a47a7fdeba820a893f
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 16:46:30 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+# Copyright 2018 Tero Karvinen http://TeroKarvinen.com GPL 3
 
-
-echo "Configuration starting..."
+echo "Sirotin Live USB configuration Starting... terokarvinen/sirotin"
 set -o verbose
 
-echo "Installing Salt and updates..."
+echo "Installing Salt and Git..."
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get -y install salt-minion
@@ -12,7 +12,9 @@ sudo apt-get -y install salt-minion
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
 
-echo "Configuring..."
+echo "Configuring Live Desktop"
 setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
 
+echo "More tips on http://terokarvinen.com/tag/saltstack"
 echo "Done."
