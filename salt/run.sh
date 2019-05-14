a8da7ac024c03dfaf341c492152cd4817ade4dbd
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 15:19:35 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
 echo "Configuration starting..."
+set -o verbose
 setxkbmap fi
+
 echo "Installing updates and Salt"
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get -y install salt-minion
+
 echo "Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
 echo 'id: koti'|sudo tee /etc/salt/minion
+
 echo "Done."
