a581de4c05f0fbefbd74a864e265a8c3a58033fe
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 14:32:35 +0300

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -3,12 +3,10 @@
 echo "Configuration starting..."
 setxkbmap fi
 
-echo "Installing Salt-Minion and DropBox..."
+echo "Installing Salt-Minion..."
 
 sudo apt-get update
 sudo apt-get -y install salt-minion
-
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 echo "Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
