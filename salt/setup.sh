c937c449c7b1ac2ca4fc4e76beb934a87034cccd
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-14 17:20:08 +0300

    Update setup.sh

--- salt/setup.sh
+++ salt/setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 echo "Configuration starting..."
+
 setxkbmap fi
+
 echo "Installing updates and salt.."
 
 sudo apt-get update
+
 sudo apt-get upgrade
 
 echo "Installing and configuring salt..."
 
 sudo apt-get -y install salt-minion
+
 echo 'master: localhost'|sudo tee /etc/salt/minion
 
 echo "Ready..Please hold for instructions given by Master."
