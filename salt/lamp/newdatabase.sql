04d2eff47a12184926a42f012d69123daf099758
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-15 10:01:58 +0300

    Update newdatabase.sql

--- salt/lamp/newdatabase.sql
+++ salt/lamp/newdatabase.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `mydb`;
 CREATE USER 'Boksi' IDENTIFIED BY 'mypassword';
 GRANT USAGE ON *.* TO 'myuser'@'%' IDENTIFIED BY 'mypassword';
-mysql> GRANT ALL privileges ON `mydb`.* TO 'Boksi'@localhost;
+GRANT ALL privileges ON `mydb`.* TO 'Boksi'@localhost;
 FLUSH PRIVILEGES;
