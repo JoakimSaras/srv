048a2e5073de27cb07f8531e342f7cc106e0eac1
JoakimSaras <49519285+JoakimSaras@users.noreply.github.com>
2019-05-15 10:05:09 +0300

    Delete newdatabase.sql

--- salt/lamp/newdatabase.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE `mydb`;
-CREATE USER 'Boksi' IDENTIFIED BY 'mypassword';
-GRANT USAGE ON *.* TO 'myuser'@'%' IDENTIFIED BY 'mypassword';
-GRANT ALL privileges ON `mydb`.* TO 'Boksi'@localhost;
-FLUSH PRIVILEGES;
